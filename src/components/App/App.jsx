191c08bc8a482730243904cf46217b32ac6a329b
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-04-12 09:38:45 +0300

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,52 @@
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm"
+
+import { useState, useEffect } from "react";
+
+
+
+export default function App() {
+
+     const getInitialContactsData = () => {
+    const savedContactsData = localStorage.getItem("contactsData");
+    return  JSON.parse(savedContactsData) 
+    }
+    
+
+
+
+ const [contacts, setContacts] = useState(getInitialContactsData);
+
+    
+    const addContacts = (newContact) => {
+        setContacts((prevContacts) => { return [...prevContacts, newContact]; });
+    };
+
+       const deleteContacts = (contactId) => {
+           setContacts((prevContacts) => { return prevContacts.filter((contact) => contact.id !== contactId);  });
+    };
+    
+    const [filter, setFilter] = useState("");
+
+
+    const visibleContacts = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+     useEffect(()=>{
+    localStorage.setItem("contactsData", JSON.stringify(contacts))
+  } ,[contacts]);
+
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            
+            <ContactForm onAdd={addContacts } />
+           
+            <SearchBox value={filter} onFilter={ setFilter} />
+            <ContactList contacts={visibleContacts } onDelete={deleteContacts} />
+        </div>
+    );
+}
+
