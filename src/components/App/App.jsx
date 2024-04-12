003da9e9688d2e771633302975daf685190f6983
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-04-12 10:24:10 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,24 @@ import ContactForm from "../ContactForm/ContactForm"
 
 import { useState, useEffect } from "react";
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const initialContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 export default function App() {
 
+    const getInitialContactData = () => {
+    const savedContactsData = localStorage.getItem("contactsData");
+    return savedContactsData ? JSON.parse(savedContactsData) : []};
+    
+    
+    
 
- const [contacts, setContacts] = useState(initialContacts);
+
+ const [contacts, setContacts] = useState(getInitialContactData);
 
     
     const addContacts = (newContact) => {
@@ -31,13 +38,7 @@ export default function App() {
   const visibleContacts = Array.isArray(contacts) ? contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter ? filter.toLowerCase() : '')
 ) : [];
-
- useEffect(() => {
-    const savedContactsData = localStorage.getItem("contactsData");
-    if (savedContactsData) {
-      setContacts(JSON.parse(savedContactsData));
-    }
-  }, []); 
+ 
 
   useEffect(() => {
     localStorage.setItem("contactsData", JSON.stringify(contacts));
